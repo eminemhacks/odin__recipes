85bacac99018bd70d3029dd272209339cc22b6fb
eminemhacks <alabura.ismail.inc@gmail.com>
2025-08-06 03:02:32 +0100

    changes to code

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php include 'templates/header.html'; ?>
+<br>
+<br>
+<br>
+<br>
+<br>
+<main class="main">
+  <section id="recipe"  class="section">
+    <div class="container" data-aos="fade-up" data-aos-delay="100">
+      <div class="row gy-4">
+        <div class="col-lg-6 order-1 order-lg-2">
+          <img src="assets/img/about.jpg" alt="" class="img-fluid about-img">
+        </div>
+        <div class="col-lg-6 order-2 order-lg-1 content">
+          <h3>Voluptatem dignissimos provident</h3>
+          <p class="fst-italic">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua.
+          </p>
+          <ul>
+            <li><i class="bi bi-check2-all"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></li>
+            <li><i class="bi bi-check2-all"></i> <span>Duis aute irure dolor in reprehenderit in voluptate velit.</span></li>
+            <li><i class="bi bi-check2-all"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</span></li>
+          </ul>
+          <p>
+            Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <h1>This is the main heading</h1>
+
+  <!-- Hero Section  -->
+  <section class="section" id="">
+    <!-- <img src="assets/img/hero-bg.png" alt="Delicious food background" data-aos-delay="50" class="hero"> -->
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-8 d-flex flex-column align-items-center align-items-lg-start text-center text-lg-start">
+          <h1 data-aos="fade-up" class="page-title" data-aos-delay="80">Our recipes</h1>
+          <p data-aos="fade-up" data-aos-delay="150">
+            Browse and view some of our recipes and learn to make the best dishes!
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Recipes Section  -->
+  
+
+
+        <br>
+        
+        <br>
+        <br>
+</main>
+
+<?php include 'templates/footer.html'; ?>
